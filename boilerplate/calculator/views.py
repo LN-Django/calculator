d846d7cc66c724388d36605bb4f6798eb37ef88d
Nathanael Hermanto <nathanael887@gmail.com>
2021-11-06 01:29:04 +0100

    fixed calculator and the urls

--- boilerplate/calculator/views.py
+++ boilerplate/calculator/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import permissions
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema    
 
 
 def calculate_tax(price):
@@ -13,8 +14,23 @@ def calculate_tax(price):
 class POSTView(APIView):
     permission_classes = [permissions.AllowAny]
 
-    def POST(request):
+    @swagger_auto_schema(request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'base_price': openapi.Schema(type=openapi.TYPE_NUMBER, description='Base price of a product'),
+        }
+    ), responses={
+        '200': openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'taxed_price': openapi.Schema(type=openapi.TYPE_NUMBER, description='Price of the product incl. tax')
+            })
+    }
+    )
+
+    def post(self, request):
         """
         Return JSON price + tax.
         """
-        return Response({'taxed_price': calculate_tax(request.body.base_price)})
+        data = request.data
+        return Response({'taxed_price': calculate_tax(data['base_price'])})
